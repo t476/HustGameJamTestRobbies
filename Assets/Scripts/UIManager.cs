80530b65509d141a0f731a914ce043c6ea9539af
t476 <57905215+t476@users.noreply.github.com>
2023-05-24 17:20:25 +0800

    Initial commit

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    static UIManager instance;
+    public TextMeshProUGUI orbText, timeText, deathText, gameOverText;
+    private void Awake()
+    {
+        if (instance != null) 
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(this);
+    }
+   public static void UpdateOrbUI(int orbCount)
+    {
+        instance.orbText.text = orbCount.ToString();
+    }
+    
+    public static void UpdateDeathUI(int deathCount)
+    {
+        instance.deathText.text = deathCount.ToString();
+    }
+    public static void UpdateTimeUI(float time)
+    {   int min = (int)(time / 60);
+        float sec = time % 60;
+        instance.timeText.text = min.ToString("00")+":"+sec.ToString("00");
+
+    }
+    public static void DisplayGameOver()
+    {
+        instance.gameOverText.enabled = true;
+
+    }
+}
